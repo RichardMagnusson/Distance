1e3f198dd373af029c9c5c6410477dfcacbbe4a6
Joel Ottosson <joel.ottosson.95@gmail.com>
2017-04-13 10:16:10 +0200

    Initial step counter implementation

---
+++ app/src/main/java/com/example/richa/distance/CounterActivity.java
@@ -0,0 +1,59 @@
+package com.example.richa.distance;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CounterActivity extends AppCompatActivity implements SensorEventListener {
+
+
+    private SensorManager sensorManager;
+    private TextView count;
+    boolean activityRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+
+        count = (TextView) findViewById(R.id.counter);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activityRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            count.setText("Count sensor not available...");
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        activityRunning = false;
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (activityRunning) {
+            count.setText(String.valueOf(event.values[0]));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}
--- app/src/main/java/com/example/richa/distance/MainActivity.java
+++ app/src/main/java/com/example/richa/distance/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.richa.distance;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    /** Called when the user taps the step counter button*/
+    public void stepCounter(View view){
+        Intent intent = new Intent(this, CounterActivity.class);
+        startActivity(intent);
     }
 }
